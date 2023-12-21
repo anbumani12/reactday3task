540ad66028aa22f34588cdb5a9cab683c9c61ac9
anbumani12 <anbumani15012002@gmail.com>
2023-12-21 14:43:34 +0530

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
-  return    <footer className="py-5 bg-dark">
-            <div className="container"><p className="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
-        </footer>
+  return (
+    <footer className="py-5 bg-dark">
+      <div className="container">
+        <p className="m-0 text-center text-white">
+          Copyright &copy; Your Website 2023
+        </p>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
